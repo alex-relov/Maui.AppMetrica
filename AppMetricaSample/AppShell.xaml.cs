eb9e05cbd9dfe46644516d3a01a0596099247f5b
Alex Relov <info@alex-relov.ru>
2025-02-23 06:29:26 +0300

    first commit

---
+++ AppMetricaSample/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AppMetricaSample;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AppMetricaSample/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AppMetricaSample;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
